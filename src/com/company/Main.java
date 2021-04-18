d555fc6f479d9faa80574a6fd27786aaf0136291
OstapVes <75254708+OstapVes@users.noreply.github.com>
2021-04-18 14:09:45 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Movie movie1 = new Movie("The Godfather", 1972, "Crime", 175, 4255, 9.2);
-        Movie movie2 = new Movie("Nice guys", 1990, "Biographical drama", 146, 1398, 8.7);
-        Movie movie3 = new Movie("Back to the future", 1985, "Adventure comedy, fantasy", 175, 4255, 8.5);
+        Movie.printStaticNumber_of_movie();
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie("Nice guys", 1990, "Biographical drama", 146, 1398, 8.7, "KINOMAN", 19.99);
+        Movie movie3 = new Movie("Back to the future", 1985, "Adventure comedy, fantasy", 175, 4255, 8.5, "SkyMall", 14.99);
         System.out.println(movie1);
         System.out.println(movie2);
         System.out.println(movie3);
+        movie1.printNumber_of_movie();
+        movie1.resetValues("The Godfather", 1972, "Crime", 175, 4255, 9.2, "Cinema Citi", 9.99);
+        System.out.println(movie1);
     }
 }
