477fc9e170f7ab0ed6ab1e45a6bc0ac76469fbd6
OstapVes <75254708+OstapVes@users.noreply.github.com>
2021-04-18 14:09:14 +0300

    Update and rename src/com/company/Movie.java to src/ком/компанії/Movie.java

--- src/ком/компанії/Movie.java
+++ src/ком/компанії/Movie.java
@@ -1,45 +1,69 @@
 package com.company;
 
 public class Movie {
-    private String name = "";
-    private int graduation_year = 0;
-    private String genre = "";
-    private int duration = 0;
-    private int number_of_reviews = 0;
-    private double imdb_rating = 0.0;
+    private String name = "The Godfather";
+    private int graduation_year = 1972;
+    private String genre = "Crime";
+    private int duration = 175;
+    private int number_of_reviews = 4255;
+    private double imdb_rating = 9.2;
+    private static int number_of_movie = 0;
+    protected String name_of_the_cinema = "Cinema Citi";
+    protected double price = 9.99;
 
+    public Movie() {
+        number_of_movie += 1;
+    }
 
-    public Movie(String name, int graduation_year, String genre, int duration, int number_of_reviews,
-                 double imdb_rating) {
+    public Movie(String name, int graduation_year, String genre, int duration) {
         this.name = name;
         this.graduation_year = graduation_year;
         this.genre = genre;
         this.duration = duration;
+        number_of_movie += 1;
+    }
+
+    public Movie(String name, int graduation_year, String genre, int duration, int number_of_reviews,
+                 double imdb_rating, String name_of_the_cinema, double price) {
+        this(name, graduation_year, genre, duration);
         this.number_of_reviews = number_of_reviews;
         this.imdb_rating = imdb_rating;
+        this.name_of_the_cinema = name_of_the_cinema;
+        this.price = price;
     }
 
     @Override
     public String toString() {
-        return "Movie{"
+        return "Movie["
                 + "Name:" + name
                 + ", Graduation year:" + graduation_year
                 + ", Genre:" + genre
                 + ", Duration:" + duration
                 + ", Number of reviews:" + number_of_reviews
                 + ", Imdb rating:" + imdb_rating
-                + "}";
+                + ", Name of the cinema:" + name_of_the_cinema
+                + ", Price:" + price + "$"
+                + "]";
     }
 
+    public static void printStaticNumber_of_movie() {
+        System.out.println("Кількість результатів: " + number_of_movie);
+    }
+
+    public void printNumber_of_movie() {
+        System.out.println("Кількість результатів: " + number_of_movie);
+    }
 
     public void resetValues(String name, int graduation_year, String genre, int duration, int number_of_reviews,
-                            double imdb_rating) {
+                            double imdb_rating, String name_of_the_cinema, double price) {
         this.name = name;
         this.graduation_year = graduation_year;
         this.genre = genre;
         this.duration = duration;
         this.number_of_reviews = number_of_reviews;
         this.imdb_rating = imdb_rating;
+        this.name_of_the_cinema = name_of_the_cinema;
+        this.price = price;
     }
 
     public String getName() {
